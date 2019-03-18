334a53473ad7c465397a6df63452a024d46609e4
akramhelil <akramhelil@gmail.com>
2019-03-18 13:05:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validate :title, presence: true
+    validate :category, include: { in: %w(Fiction Non-Fiction)}
+    validate :content, length: { minimun: 100 }
 end
